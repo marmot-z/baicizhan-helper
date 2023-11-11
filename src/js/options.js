11b3a1afc2994868d5764dc1fc9f8cbe53d28431
zhangxunwei <zhangxunweia@gmail.com>
2023-11-11 20:59:17 +0800

    feat: remove join us link

--- src/js/options.js
+++ src/js/options.js
@@ -12,14 +12,6 @@
         let event = accessToken ? events.AUTHED : events.UNAUTHED;
 
         $doc.trigger(event);
-
-        $('#joinLink').on('click', (e) => {
-            e.preventDefault();            
-            alert(
-                '如果你对插件使用有疑问，或是要提些建议，亦或是单纯的想交个朋友，那就快加入我们的讨论群吧！\n' +
-                '添加我的微信：zxw1536711318（请备注：百词斩助手）。'
-            );
-        });
     }
 
     window.onload = init;
