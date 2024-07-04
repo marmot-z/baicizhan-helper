40d52749e935bbd465e9e278298103b1967e1282
zhangxunwei <zhangxunweia@gmail.com>
2024-07-04 21:29:33 +0800

    feat: show wechat qrCode

--- src/js/options.js
+++ src/js/options.js
@@ -3,10 +3,23 @@
 
     const $doc = $(document);
 
+    function initWechatQrCode() {
+        storageModule.get(['host', 'port'])
+        .then(([host, port]) => {
+            $('#qrCodeImg').webuiPopover({
+                placement: 'left',
+                type: 'html',
+                trigger: 'hover',
+                content: `<img src="http://${host}:${port}/qrcode.jpg" style="width: 250px; height: 375px;"/>`
+            });
+        });
+    }
+
     async function init() {
         loginModule.init();
         settingModule.init();
         wordbookModule.init();
+        initWechatQrCode();
         
         let accessToken = await storageModule.get('accessToken');
         let event = accessToken ? events.AUTHED : events.UNAUTHED;
